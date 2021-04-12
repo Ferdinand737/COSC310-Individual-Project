3a1654b2286ca6aa30a7124f340a221a99f8c8bf
Ferdinand737 <ferdinandhaaben@gmail.com>
2021-04-12 12:00:06 -0700

    done

---
+++ src/core/Test.java
@@ -0,0 +1,10 @@
+package core;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+public class Test {
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+		Chatbot chatbot = new Chatbot();
+		
+	}
+}
